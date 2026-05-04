159a218856765da54382194791d126244d8c97be
Aliaksandr Shabanovich <alikshabanovich@gmail.com>
2026-05-05 00:47:46 +0300

    feat: add Error Boundary

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import { CardsList } from './components/CountriesCardsList/CountriesCardsList';
 import { Search } from './components/Search/Search';
+import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
+import { ErrorSimulator } from './components/ErrorSimulator/ErrorSimulator';
 
 interface AppState {
   searchStr: string;
+  simulateError: boolean;
 }
 
 class App extends Component<{}, AppState> {
@@ -12,12 +15,19 @@ class App extends Component<{}, AppState> {
     super(props);
     this.state = {
       searchStr: localStorage.getItem('searchStr') || '',
+      simulateError: false,
     };
   }
 
   handleSearchChange = (newStr: string) => {
     this.setState({ searchStr: newStr });
     localStorage.setItem('searchStr', newStr);
+  };
+
+  toggleError = () => {
+    this.setState((prevState) => ({
+      simulateError: !prevState.simulateError,
+    }));
   };
 
   componentWillUnmount() {
@@ -27,11 +37,36 @@ class App extends Component<{}, AppState> {
   render() {
     return (
       <>
-        <Search
-          searchStr={this.state.searchStr}
-          onSearchChange={this.handleSearchChange}
-        />
-        <CardsList searchStr={this.state.searchStr} />
+        <button
+          onClick={this.toggleError}
+          style={{
+            margin: '1rem',
+            padding: '0.5rem 1rem',
+            background: '#ff6b6b',
+            color: 'white',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer',
+          }}
+        >
+          {this.state.simulateError
+            ? 'Reset Error Simulation'
+            : 'Test Error Boundary'}
+        </button>
+
+        <ErrorBoundary>
+          {this.state.simulateError ? (
+            <ErrorSimulator />
+          ) : (
+            <>
+              <Search
+                searchStr={this.state.searchStr}
+                onSearchChange={this.handleSearchChange}
+              />
+              <CardsList searchStr={this.state.searchStr} />
+            </>
+          )}
+        </ErrorBoundary>
       </>
     );
   }
